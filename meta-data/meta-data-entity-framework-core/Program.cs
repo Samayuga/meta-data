b3e2280890c686b790f98eadea1359089f033793
Rahul Uttarkar <rahul.a.uttarkar@gmail.com>
2020-07-26 14:01:18 +0530

    removing the library project

---
+++ meta-data/meta-data-entity-framework-core/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetaDataEntityFrameworkCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Test");
+            Console.ReadLine();
+        }
+    }
+}
