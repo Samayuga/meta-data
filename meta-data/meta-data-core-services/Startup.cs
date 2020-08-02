4a722dc9162195b6d93f829cedc4af5abd50f3a2
Rahul Uttarkar <rahul.a.uttarkar@gmail.com>
2020-08-02 14:12:25 +0530

    _

--- meta-data/meta-data-core-services/Startup.cs
+++ meta-data/meta-data-core-services/Startup.cs
@@ -29,9 +29,7 @@ namespace MetaDataCoreServices
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
             else
             {
                 app.UseExceptionHandler("/Error");
