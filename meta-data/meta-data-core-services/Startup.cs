4d5f9b4c57aa4716f31490d25a17887ac5c6cbba
Rahul Uttarkar <rahul.a.uttarkar@gmail.com>
2020-08-02 10:14:35 +0530

    Removing comments

--- meta-data/meta-data-core-services/Startup.cs
+++ meta-data/meta-data-core-services/Startup.cs
@@ -17,18 +17,15 @@ namespace MetaDataCoreServices
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -38,7 +35,6 @@ namespace MetaDataCoreServices
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -60,9 +56,6 @@ namespace MetaDataCoreServices
 
             app.UseSpa(spa =>
             {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
                 spa.Options.SourcePath = "ClientApp";
 
                 if (env.IsDevelopment())
