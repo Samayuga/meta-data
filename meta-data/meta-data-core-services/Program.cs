e779315a7505424276f07abff337aae59cb48731
Rahul Uttarkar <rahul.a.uttarkar@gmail.com>
2020-08-02 19:19:27 +0530

    Code cleaning

--- meta-data/meta-data-core-services/Program.cs
+++ meta-data/meta-data-core-services/Program.cs
@@ -16,38 +16,27 @@ namespace MetaDataCoreServices
     {
         public static void Main(string[] args)
         {
-
             var host = CreateHostBuilder(args).Build();
-           
-            CreateDbIfNotExists(host);
-
+            SeedDatabase(host);
             host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
 
-        private static void CreateDbIfNotExists(IHost host)
+        private static void SeedDatabase(IHost host)
         {
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
+            using var serviceScope = host.Services.CreateScope();
+            var serviceProvider = serviceScope.ServiceProvider;
 
-                try
-                {
-                    var context = services.GetRequiredService<MetaDataDatabaseContext>();
-                    DatabaseInitializer.Initialize(context);
-                    //context.Database.EnsureCreated();
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred creating the DB.");
-                }
+            try
+            {
+                var context = serviceProvider.GetRequiredService<MetaDataDatabaseContext>();
+                DatabaseInitializer.Initialize(context);
+            }
+            catch (Exception exception)
+            {
+                var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+                logger.LogError(exception, exception.Message);
             }
         }
     }
